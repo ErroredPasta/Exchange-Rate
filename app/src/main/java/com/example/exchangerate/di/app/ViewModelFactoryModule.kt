690da1a432a6dd68e4915b0afb2de3fff422bdca
ErroredPasta <ForeignServices3132@pm.me>
2023-01-14 19:04:26 +0900

    ViewModelFactoryModule.kt에 subcomponent 지정

--- app/src/main/java/com/example/exchangerate/di/app/ViewModelFactoryModule.kt
+++ app/src/main/java/com/example/exchangerate/di/app/ViewModelFactoryModule.kt
@@ -1,12 +1,14 @@
 package com.example.exchangerate.di.app
 
 import androidx.lifecycle.ViewModelProvider
+import com.example.exchangerate.di.activity.MainActivityComponent
 import com.example.exchangerate.presentation.MainViewModel
 import dagger.Binds
 import dagger.Module
 
-@Module
+@Module(subcomponents = [MainActivityComponent::class])
 interface ViewModelFactoryModule {
+
     @Binds
     fun bindMainViewModelFactory(factory: MainViewModel.Factory): ViewModelProvider.Factory
 }
